d8909c2ca147804aecace8bc31e8a9680ba462f3
edward <edbryson7@gmail.com>
2022-04-29 19:00:54 -0400

    comments

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import numpy as np
 import pandas as pd
 import cv2
@@ -11,13 +12,15 @@ from tensorflow.keras import layers, models
 from heatmapplot import *
 
 def main():
-    # np.set_printoptions(threshold=sys.maxsize)
+    '''
+    Main function for the image classification
+    '''
+
+    # Pulling mode type from command-line arguments
     try:
         mode = int(sys.argv[1])
     except:
         mode = 0
-
-    mode = 2
 
     train_path = './/casting_data//train//'
     test_path = './/casting_data//test//'
@@ -29,24 +32,31 @@ def main():
     testx, testy = init_df(testdf, test_path)
     testx = np.expand_dims(testx, axis=-1) # <--- add batch axisp
 
-    # print(np.shape(trainx))
-    # print(np.shape(trainy))
-
     if mode == 1:
+        # Create and train the model
         model = init_cnn(np.shape(trainx[0]))
         train_cnn(model, trainx, trainy, testx, testy)
     elif mode == 2:
+        # Test the model
         test_cnn(testx, testy)
     else:
+        # Create, train and test the model
         model = init_cnn(np.shape(trainx[0]))
         train_cnn(model, trainx, trainy, testx, testy)
         test_cnn(testx, testy)
 
 
-
 def init_cnn(inp_shape):
-    print('='*90)
-    print(inp_shape)
+    """
+    Create a CNN model from an input shape
+
+    Parameters
+    ----------
+    inp_shape
+        A list of the dimensions
+
+    """
+
     model = models.Sequential()
 
     model.add(layers.Conv2D(8, (3, 3), activation='relu', input_shape=inp_shape))
@@ -71,17 +81,31 @@ def init_cnn(inp_shape):
               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-    # model.compile(loss='binary_crossentropy',optimizer='adam',metrics='accuracy')
     return model
 
-def train_cnn(model, images, labels, test_images, test_labels):
-    # model.summary()
 
-    print('='*90)
+def train_cnn(model, images, labels, test_images, test_labels):
+    """
+    Train the model from a training set and testing set
+
+    Parameters
+    ----------
+    model
+        A tensorflow model
+    images
+        A numpy array of images to train with
+    labels
+        A numpy array of classification labels
+
+    test_images
+        A numpy array of images to validate
+    test_labels
+        A numpy array of classification labels
+    """
+
     history = model.fit(images, labels, epochs=15, batch_size=50, verbose=1,
                     validation_data=(test_images, test_labels))
 
-    # print('Finished training')
     model.save('./model')
 
     plt.plot(history.history['accuracy'], label='accuracy')
@@ -104,7 +128,20 @@ def train_cnn(model, images, labels, test_images, test_labels):
     plt.show()
     return model
 
+
 def test_cnn(test_images, test_labels):
+    """
+    Test the CNN performance from the testing set
+
+    Parameters
+    ----------
+    test_images
+        A numpy array of images to test with
+    test_labels
+        A numpy array of classification labels
+    """
+
+    # Load the model
     model = tf.keras.models.load_model('./model')
     model.summary()
 
@@ -122,7 +159,7 @@ def test_cnn(test_images, test_labels):
     assert len(y)==len(test_labels)
 
     # Creating a heatmap
-    heat = np.zeros((2,2))#,dtype=int)
+    heat = np.zeros((2,2))
     for i in range(len(y)):
         heat[y[i],test_labels[i]]+=1
 
@@ -143,20 +180,33 @@ def test_cnn(test_images, test_labels):
     plt.show()
 
 
-
 def init_df(df, path):
+    """
+    Creating the numpy array of images from the dataframe
+    
+    Parameters
+    ----------
+    df
+        A dataframe containing image filenames and labels
+    path
+        A string pointing to the directory of the images
+    """
+
+    # Randomizing the order of the images
     df = df.sample(frac=1)
     files = df['image'].to_numpy()
     labels = df['label'].to_numpy()
 
     images = np.empty([len(files),300,300])
 
+    # Adding the images to the numpy array
     for i in range(len(files)):
         im = cv2.imread(path+files[i])
         im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         images[i,:,:] = im
 
     return images, labels.flatten()
+
 
 if __name__ == '__main__':
     main()
